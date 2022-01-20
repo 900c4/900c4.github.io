55d375288f913bf45b8b034984c10719810af43e
900c4 <66842688+900c4@users.noreply.github.com>
2022-01-20 23:56:23 +0900

    Add files via upload

---
+++ static/js/main.f61581c8.chunk.js
@@ -0,0 +1,16 @@
+(this.webpackJsonppoooo=this.webpackJsonppoooo||[]).push([[0],{12:function(e,t,n){"use strict";n.r(t);var c=n(1),i=n.n(c),o=n(3),s=n.n(o),r=(n(9),n(4)),a=n.n(r),u=n(0);
+
+function l(){return Object(u.jsx)(a.a,{channel:"izonelove10291",layout:"video",width:"100%",height:"100%"})}//channel:도방할 트위치 채널명
+function g(){return Object(u.jsx)("div",{className:"gutter gutter-horizontal"})}
+function d(){return Object(c.useEffect)((function(){var e=document.createElement("script");return e.src="//client.uchat.io/uchat.js",e.async=!0,
+
+document.body.append(e),function(){e.remove()}}),[]),Object(u.jsx)("u-chat",{room:"oozy1201",style:{width:"100%",height:"100%"}})}
+
+var h=function(){return Object(u.jsx)("div",{className:"container max-w-full h-full",children:Object(u.jsxs)("div",{className:"md:flex h-full",
+children:[Object(u.jsx)("div",{className:"h-3/6 md:flex-1 md:h-screen",children:Object(u.jsx)(l,{})}),//트위치플레이어가 들어갈자리
+Object(u.jsx)(g,{}),
+Object(u.jsx)("div",{className:"h-3/6 md:flex-initial md:h-screen",children:Object(u.jsx)(d,{})})]})})},//채팅창이 들어갈 자리
+j=function(e){e&&e instanceof Function&&n.e(3).then(n.bind(null,13)).then((function(t){
+    var n=t.getCLS,c=t.getFID,i=t.getFCP,o=t.getLCP,s=t.getTTFB;n(e),c(e),i(e),o(e),s(e)}))};s.a.render(Object(u.jsx)(i.a.StrictMode,{children:Object(u.jsx)(h,{})}),
+    document.getElementById("root")),j()},9:function(e,t,n){}},[[12,1,2]]]);
+//# sourceMappingURL=main.f61581c8.chunk.js.map
